e40aa348eaf3dc69d1ef07cd2d1ea0ea85542948
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2022-12-12 12:31:45 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.dropdown-toggle').on('click', function(){
-        $('.dropdown-menu').show();
-    });
-    $('main').on('mouseover', function(){
-        $('.dropdown-menu').hide();
-    });
-});
